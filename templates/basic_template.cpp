61daccd5d1b157b5c67765ce7108d0880967107c
hsmatsuura2006-web <h.s.matsuura.2006@gmail.com>
2026-01-15 18:59:27 +0900

    Update basic_template.cpp
    コンパイルオプションはオンライン実行時に指定しています。

--- templates/basic_template.cpp
+++ templates/basic_template.cpp
@@ -1,1 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <map>
+#include <set>
+#include <utility>
+#include <iomanip>
+#include <limits>
+#include <cstdint>
+#ifdef ONLINE_JUDGE
+#define dbg(x) (void)0
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#else
+#define dbg(x) cerr << #x << " = " << (x) << " (L" << __LINE__ << ")" << endl;
+#endif
+#define rep(i,n) for(ll i=0;i<n;i++)
+#define rep1(i,n) for(ll i=1;i<=n;i++)
+#define yes cout << "Yes" << endl;
+#define no cout << "No" << endl;
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using lll = __int128;
+using ld = long double;
+using P = pair<ll,ll>;
+using Graph = vector<vector<ll>>;
+void yn(bool f) { cout << (f ? "Yes" : "No") << "\n"; }
+template<typename T> using vc = vector<T>;
+template<typename T> using vv = vc<vc<T>>;
+using vl = vc<ll>; using vvl = vv<ll>; using vvvl = vv<vl>; using vvvvl = vv<vvl>;
+using vs = vc<string>; using vvs = vv<string>;
+template<class T> using pq = priority_queue<T, vector<T>>;//降順
+template<class T> using pq_g = priority_queue<T, vector<T>, greater<T>>;//昇順
+//const ll dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+//bool out_grid(const ll i, const ll j, const ll h, const ll w) {return(!(0 <= i && i < h && 0 <= j && j < w));}
+void solve() {
 
+
+}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    ll T = 1;
+    while (T--) {solve();}
+    return 0;
+}
