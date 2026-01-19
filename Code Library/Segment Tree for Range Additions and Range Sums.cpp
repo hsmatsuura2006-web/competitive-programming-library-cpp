2fe3c717e2f464e5f3d70f2a80d1fa715b269f08
shin0830 <h.s.matsuura.2006@gmail.com>
2026-01-19 10:39:02 +0900

    Update Segment Tree for Range Additions and Range Sums.cpp
    通常のセグメント木で範囲加算ができるようにしました。

--- Code Library/Segment Tree for Range Additions and Range Sums.cpp
+++ Code Library/Segment Tree for Range Additions and Range Sums.cpp
@@ -1,1 +1,32 @@
+struct SegmentTree {
+  ll n;
+  vector<ll> data, datb;
+  SegmentTree(int _n) {
+    n = 1;
+    while (n < _n) n <<= 1;
+    data.assign(2 * n, 0);
+    datb.assign(2 * n, 0);
+  }
+  void add(ll a, ll b, ll x, ll k, ll l, ll r) {
+    if (a <= l && r <= b) {
+      data[k] += x;
+    }else if (l < b && a < r) {
+      datb[k] += (min(b, r) - max(a, l)) * x;
+      add(a, b, x, k * 2 + 1, l, (l + r)/2);
+      add(a, b, x, k * 2 + 2, (l + r)/2, r);
+    }
+  }
 
+  ll sum(ll a, ll b, ll k, ll l, ll r) {
+    if (b <= l || r <= a) {
+      return 0;
+    }else if (a <= l && r <= b) {
+      return data[k] * (r - l) + datb[k];
+    }else {
+      ll res = (min(b, r) - max(a, l)) * data[k];
+      res += sum(a, b, k * 2 + 1, l, (l + r)/2);
+      res += sum(a, b, k * 2 + 2, (l + r)/2, r);
+      return res;
+    }
+  }
+};
