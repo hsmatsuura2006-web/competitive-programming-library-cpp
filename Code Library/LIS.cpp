4b11f6e4239b6d8b77666119a7afc7a8224b3b9e
shin0830 <h.s.matsuura.2006@gmail.com>
2026-01-24 11:37:51 +0900

    Update LIS.cpp
    連続でなくてもよい狭義単調増加の数列の最大の長さを求めます。

--- Code Library/LIS.cpp
+++ Code Library/LIS.cpp
@@ -1,1 +1,13 @@
-
+ll calculateLIS(const vl& a) {
+    if (a.empty()) return 0;
+    vl lis;
+    for (const auto& x : a) {
+        auto it = lower_bound(lis.begin(), lis.end(), x);
+        if (it == lis.end()) {
+            lis.push_back(x);
+        } else {
+            *it = x;
+        }
+    }
+    return lis.size();
+}
