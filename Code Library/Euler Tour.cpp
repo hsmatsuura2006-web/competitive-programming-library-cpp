be1f1383cc193192cc75cd8eca3cfd979d306bb3
shin0830 <h.s.matsuura.2006@gmail.com>
2026-01-27 14:49:37 +0900

    Update Euler Tour.cpp
    重み付きグラフにしました

--- Code Library/Euler Tour.cpp
+++ Code Library/Euler Tour.cpp
@@ -3,20 +3,23 @@ struct EulerTour {
     vector<int> depths;       
     vector<int> first_pos;//クエリの範囲を記録  
     vector<int> last_pos;  
-    EulerTour(int n, const vector<vector<int>>& adj, int root = 0) {
+    vector<ll> dist_from_root;
+    EulerTour(int n, const vector<vector<Edge>>& adj, int root = 0) {
+				dist_from_root.assign(n, -1);
         tour.reserve(2 * n);
         depths.reserve(2 * n);
         first_pos.assign(n, -1);
         last_pos.assign(n, -1);
-        dfs(root, -1, 0, adj);
+        dfs(root, -1, 0, adj, 0);
     }
-    void dfs(int u, int p, int d, const vector<vector<int>>& adj) {
+    void dfs(int u, int p, int d, const vector<vector<Edge>>& adj, ll current_dist) {
         first_pos[u] = tour.size();
         tour.push_back(u);
         depths.push_back(d);
-        for (int v : adj[u]) {
-            if (v != p) {
-                dfs(v, u, d + 1, adj);
+        dist_from_root[u] = current_dist;
+        for (Edge v : adj[u]) {
+            if (v.to != p) {
+                dfs(v.to, u, d + 1, adj, current_dist + v.cost);
                 tour.push_back(u);
                 depths.push_back(d);
             }
