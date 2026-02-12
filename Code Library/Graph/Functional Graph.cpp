bf608bc496e3c2bd3128d9a9022434457a527979
shin0830 <h.s.matsuura.2006@gmail.com>
2026-02-12 23:17:29 +0900

    Update Functional Graph.cpp

--- Code Library/Graph/Functional Graph.cpp
+++ Code Library/Graph/Functional Graph.cpp
@@ -1,41 +1,36 @@
+#include <vector>
+#include <queue>
 /**
  * Functional Graph の閉路抽出
  * @param next_node: 各頂点の遷移先
  * @return cycles: 各閉路を構成する頂点リストの集合
  * @return is_cycle_member: 各頂点が閉路に属するかどうかのフラグ
  */
-templete<type T>
+template<typename T> 
 struct FunctionalGraph {
     int n;
     std::vector<T> next_node;
     std::vector<std::vector<T>> cycles;
     std::vector<bool> is_cycle_member;
-
     FunctionalGraph(const std::vector<T>& next) : n(next.size()), next_node(next), is_cycle_member(n, true) {
-        std::vector<T> in_degree(n, 0);
+        std::vector<int> in_degree(n, 0);
         for (int i = 0; i < n; ++i) in_degree[next_node[i]]++;
-
-        // 1. トポロジカルソートで閉路以外の枝を削除
         std::queue<T> que;
         for (int i = 0; i < n; ++i) {
             if (in_degree[i] == 0) que.push(i);
         }
-
         while (!que.empty()) {
-            int v = que.front();
+            T v = que.front(); 
             que.pop();
-            is_cycle_member[v] = false; // 枝なので閉路ではない
-            int nv = next_node[v];
+            is_cycle_member[v] = false; 
+            T nv = next_node[v];
             if (--in_degree[nv] == 0) que.push(nv);
         }
-
-        // 2. 閉路の抽出
         std::vector<bool> visited(n, false);
         for (int i = 0; i < n; ++i) {
             if (!is_cycle_member[i] || visited[i]) continue;
-            
             std::vector<T> cycle;
-            int curr = i;
+            T curr = i;
             while (!visited[curr]) {
                 visited[curr] = true;
                 cycle.push_back(curr);
