d822ee6a4bf877a68e9cf5900e355b4e40eac24e
shin0830 <h.s.matsuura.2006@gmail.com>
2026-04-17 14:31:19 +0900

    Update ZobristHash.cpp

--- Code Library/ZobristHash.cpp
+++ Code Library/ZobristHash.cpp
@@ -1,11 +1,9 @@
-// --- Zobrist Hash テンプレート開始 ---
 struct ZobristHash {
     mt19937_64 engine;
     unordered_map<ll, ull> val_to_hash;
 
     ZobristHash() : engine(chrono::steady_clock::now().time_since_epoch().count()) {}
 
-    // 数値に対して一意なランダム背番号を返す
     ull get_id(ll x) {
         if (val_to_hash.find(x) == val_to_hash.end()) {
             val_to_hash[x] = engine();
@@ -13,14 +11,12 @@ struct ZobristHash {
         return val_to_hash[x];
     }
 
-    // 配列の各地点における「集合ハッシュ」の累積配列を返す
     vector<ull> build_prefix_hashes(const vl& a) {
         int n = a.size();
         vector<ull> res(n);
         set<ll> seen;
         ull cur = 0;
         for (int i = 0; i < n; i++) {
-            // 初めて登場した時だけXORをとる
             if (seen.find(a[i]) == seen.end()) {
                 cur ^= get_id(a[i]);
                 seen.insert(a[i]);
@@ -30,4 +26,3 @@ struct ZobristHash {
         return res;
     }
 };
-// --- Zobrist Hash テンプレート終了 ---
